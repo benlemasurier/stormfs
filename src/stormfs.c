5992917e6abc6039abf0a3bc73f2b0ac0992851b
Ben LeMasurier <ben.lemasurier@gmail.com>
2011-11-08 13:17:03 -0700

    -ENOTSUP on unsupported operations

--- src/stormfs.c
+++ src/stormfs.c
@@ -20,27 +20,27 @@ static struct fuse_operations stormfs_oper;
 static int
 stormfs_getattr(const char *path, struct stat *stbuf)
 {
-  return -ENOENT;
+  return -ENOTSUP;
 }
 
 static int
 stormfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, 
                            off_t offset, struct fuse_file_info *fi)
 {
-  return -ENOENT;
+  return -ENOTSUP;
 }
 
 static int
 stormfs_open(const char *path, struct fuse_file_info *fi)
 {
-  return -ENOENT;
+  return -ENOTSUP;
 }
 
 static int
 stormfs_read(const char *path, char *buf, size_t size, off_t offset,
                         struct fuse_file_info *fi)
 {
-  return -ENOENT;
+  return -ENOTSUP;
 }
 
 int
