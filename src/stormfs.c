2ec0bd18c4a2330032fb450d68f884043927f8e3
Ben LeMasurier <ben.lemasurier@gmail.com>
2011-11-08 14:19:20 -0700

    FUSE option parser

--- src/stormfs.c
+++ src/stormfs.c
@@ -9,13 +9,47 @@
 #define FUSE_USE_VERSION 26
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <fuse.h>
 #include "stormfs.h"
 
-static struct fuse_operations stormfs_oper;
+enum {
+  KEY_HELP,
+  KEY_VERSION,
+  KEY_FOREGROUND,
+};
+
+struct stormfs {
+  int foreground;
+  int url;
+};
+
+static struct stormfs stormfs;
+
+#define STORMFS_OPT_KEY(t, p, v) { t, offsetof(struct stormfs, p), v }
+static struct fuse_opt stormfs_opts[] = 
+{
+  // STORMFS_OPT_KEY("url=%s",   url,  0),
+
+  FUSE_OPT_KEY("-f",            KEY_FOREGROUND),
+  FUSE_OPT_KEY("--foreground",  KEY_FOREGROUND),
+  FUSE_OPT_KEY("-h",            KEY_HELP),
+  FUSE_OPT_KEY("--help",        KEY_HELP),
+  FUSE_OPT_KEY("-V",            KEY_VERSION),
+  FUSE_OPT_KEY("--version",     KEY_VERSION),
+  FUSE_OPT_END
+};
+
+static struct 
+fuse_operations stormfs_oper = {
+    .getattr  = stormfs_getattr,
+    .readdir  = stormfs_readdir,
+    .open     = stormfs_open,
+    .read     = stormfs_read,
+};
 
 static int
 stormfs_getattr(const char *path, struct stat *stbuf)
@@ -43,16 +77,25 @@ stormfs_read(const char *path, char *buf, size_t size, off_t offset,
   return -ENOTSUP;
 }
 
+static int
+stormfs_fuse_main(struct fuse_args *args)
+{
+  return fuse_main(args->argc, args->argv, &stormfs_oper, NULL);
+}
+
 int
 main(int argc, char *argv[])
 {
-  // map FUSE bindings
-  memset(&stormfs_oper, 0, sizeof(stormfs_oper));
-  stormfs_oper.getattr = stormfs_getattr;
-  stormfs_oper.readdir = stormfs_readdir;
-  stormfs_oper.open    = stormfs_open;
-  stormfs_oper.read    = stormfs_read;
+  int status;
+  struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
 
-  // hand off control to FUSE
-  return fuse_main(argc, argv, &stormfs_oper, NULL);
+  memset(&stormfs, 0, sizeof(struct stormfs));
+  if(fuse_opt_parse(&args, &stormfs, stormfs_opts, NULL) == -1)
+    return EXIT_FAILURE;
+
+  status = fuse_main(args.argc, args.argv, &stormfs_oper, NULL);
+
+  fuse_opt_free_args(&args);
+
+  return status;
 }
--- src/stormfs.h
+++ src/stormfs.h
@@ -8,4 +8,5 @@ static int stormfs_open(const char *path, struct fuse_file_info *fi);
 static int stormfs_read(const char *path, char *buf, size_t size, off_t offset,
                         struct fuse_file_info *fi);
 
+static int stormfs_fuse_main(struct fuse_args *args);
 #endif // stormfs_H
