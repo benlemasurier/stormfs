c6e2dcae65496805df559a69317fa076c1d76063
Ben LeMasurier <ben.lemasurier@gmail.com>
2011-11-08 13:08:43 -0700

    cleaning up gpl display

--- src/stormfs.c
+++ src/stormfs.c
@@ -1,20 +1,9 @@
 /*
  * stormfs - A FUSE abstraction layer for cloud storage
  * Copyright (C) 2011 Ben LeMasurier <ben.lemasurier@gmail.com>
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * This program can be distributed under the terms of the GNU GPL.
+ * See the file COPYING.
  */
 
 #define FUSE_USE_VERSION 26
@@ -24,8 +13,9 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <fuse.h>
-
 #include "stormfs.h"
+
+static struct fuse_operations stormfs_oper;
 
 static int
 stormfs_getattr(const char *path, struct stat *stbuf)
--- src/stormfs.h
+++ src/stormfs.h
@@ -1,8 +1,6 @@
 #ifndef stormfs_H
 #define stormfs_H
 
-static struct fuse_operations stormfs_oper;
-
 static int stormfs_getattr(const char *path, struct stat *stbuf);
 static int stormfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, 
                            off_t offset, struct fuse_file_info *fi);
