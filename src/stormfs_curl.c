1cffe05580071905dfaa33e442e731c9948b9079
Ben LeMasurier <ben.lemasurier@gmail.com>
2011-11-09 16:52:15 -0700

    broken request signing (cryptoisfuckinghard.)

--- src/stormfs_curl.c
+++ src/stormfs_curl.c
@@ -11,9 +11,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
 #include <curl/curl.h>
 #include <curl/types.h>
 #include <curl/easy.h>
+#include <gcrypt.h>
 #include "stormfs_curl.h"
 
 struct stormfs_curl {
@@ -26,8 +28,65 @@ struct stormfs_curl_memory {
 };
 
 static int
+stormfs_curl_sign_request(struct curl_slist *headers, const char *path)
+{
+  // TODO:
+  char *to_sign = "TEST"; 
+  const char *key = "TEST";
+
+  int mdlen;
+  size_t n_bytes;
+  gcry_error_t error = 0;
+  gcry_md_hd_t digest = NULL;
+  unsigned char *md_string = NULL;
+
+  error = gcry_md_open(&digest, GCRY_MD_SHA1, GCRY_MD_FLAG_HMAC);
+  if(error) {
+    fprintf(stderr, "stormfs: %s\n", gcry_strerror(error));
+    abort();
+  }
+
+  mdlen = gcry_md_get_algo_dlen(GCRY_MD_SHA1);
+
+  error = gcry_md_setkey(digest, key, strlen(key));
+  if(error) {
+    fprintf(stderr, "stormfs: %s\n", gcry_strerror(error));
+    abort();
+  }
+
+  gcry_md_write(digest, to_sign, strlen(to_sign));
+  md_string = gcry_md_read(digest, 0);
+
+  int i;
+  for(i = 0; i < mdlen; i++)
+    printf("%02x ", md_string[i] & 0xFF);
+  printf("\n");
+
+  gcry_md_close(digest);
+
+  printf("SIGNATURE: %s\n", md_string);
+
+  return 0;
+}
+
+static const char *
+stormfs_curl_rfc2822_timestamp()
+{
+  char s[40];
+  char *date;
+
+  time_t t = time(NULL);
+  strftime(s, sizeof(s), "Date: %a, %d %b %Y %T %z", gmtime(&t));
+
+  date = strdup(s);
+
+  return date;
+}
+
+static int
 stormfs_curl_set_defaults(CURL *c)
 {
+  // FIXME: why not work?
   curl_easy_setopt(c, CURLOPT_NOPROGRESS, 1L);
   curl_easy_setopt(c, CURLOPT_USERAGENT, "stormfs");
 
@@ -75,6 +134,8 @@ stormfs_curl_init(const char *url)
   if((result = curl_global_init(CURL_GLOBAL_ALL)) != CURLE_OK)
     return -1;
 
+  gcry_control(GCRYCTL_DISABLE_SECMEM, 0);
+
   return 0;
 }
 
@@ -83,25 +144,32 @@ stormfs_curl_get(const char *path)
 {
   CURL *c;
   char *url = stormfs_curl_get_url(path);
+  struct curl_slist *headers = NULL; 
   struct stormfs_curl_memory data;
   data.memory = malloc(1);
   data.size = 0;
+
+  headers = curl_slist_append(headers, stormfs_curl_rfc2822_timestamp());
+  stormfs_curl_sign_request(headers, path);
 
   c = curl_easy_init();
   stormfs_curl_set_defaults(&c);
   curl_easy_setopt(c, CURLOPT_URL, url);
   curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, stormfs_curl_write_memory_cb);
   curl_easy_setopt(c, CURLOPT_WRITEDATA, (void *) &data);
+  curl_easy_setopt(c, CURLOPT_HTTPHEADER, headers);
+  curl_easy_setopt(c, CURLOPT_VERBOSE, 1L);
 
   curl_easy_perform(c);
 
-  // printf("DATA:\n%s\n", data.memory);
+  printf("DATA:\n%s\n", data.memory);
 
   if(data.memory)
     free(data.memory);
 
   free(url);
   curl_easy_cleanup(c);
+  curl_slist_free_all(headers);
 
   return 0;
 }
