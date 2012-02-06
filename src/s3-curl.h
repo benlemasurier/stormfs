b3be497fdf354663d74b65a5f948619a32824a95
Ben LeMasurier <ben.lemasurier@gmail.com>
2012-02-06 13:45:28 -0700

    s3 curl abstraction (skel)

---
+++ src/s3-curl.h
@@ -0,0 +1,12 @@
+/*
+ * stormfs - A FUSE abstraction layer for cloud storage
+ * Copyright (C) 2011 Ben LeMasurier <ben.lemasurier@gmail.com>
+ *
+ * This program can be distributed under the terms of the GNU GPL.
+ * See the file COPYING.
+ */
+
+#ifndef s3_curl_H
+#define s3_curl_H
+
+#endif // s3_curl_H
